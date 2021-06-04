b15a3c55bca3bbf98f0bb15d881b399e7ae19001
simrath randhawa <srandhawa1250@gmail.com>
2021-06-03 22:39:09 -0400

    capstone changes

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -4,6 +4,10 @@ import com.techelevator.view.Menu;
 
 public class VendingMachineCLI {
 
+	// private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+	//	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	//	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction"
+
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
@@ -11,6 +15,7 @@ public class VendingMachineCLI {
 	private Menu menu;
 
 	public VendingMachineCLI(Menu menu) {
+
 		this.menu = menu;
 	}
 
@@ -22,6 +27,14 @@ public class VendingMachineCLI {
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+				//} while (true) {
+				//String purchaseChoice = (String) menu.getChoiceFromOptionsPurchaseMenu(PURCHASE_MENU_OPTIONS);
+				// if (purchaseChoice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
+				// feed vending machine money
+			// } else if (purchaseChoice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
+				// select the product
+				//} else if (purchaseChoice.equals(PURCHASE_MENU_OPTION_FINISH_TRANSACTION));
+				// finish transaction
 			}
 		}
 	}
